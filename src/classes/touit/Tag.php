ac8e1d34112ff669f0b41b9425f8afbe3a8e5971
Ahmed <ahmedcheklat@outlook.com>
2023-11-07 09:07:04 +0100

    Ajout d'un constructeur et de méthode dans Tag.php

--- src/classes/touit/Tag.php
+++ src/classes/touit/Tag.php
@@ -4,5 +4,31 @@ namespace iutnc\touiteur\touit;
 
 class Tag
 {
+    private array $listTouits = [];
+
+    /**
+     * @param array $listTouits
+     */
+    public function __construct(array $listTouits)
+    {
+        $this->listTouits = $listTouits;
+    }
+
+    /**
+     * @param Touit $t
+     * Ajoute un touit à la liste des touits
+     */
+    public function ajoutTouit(Touit $t): void
+    {
+        $this->listTouits[] = $t;
+    }
+
+    /**
+     * @return array
+     * Retourne la liste des touits
+     */
+    public function getTouits(): array {
+        return $this->listTouits;
+    }
 
 }
