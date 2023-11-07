49c8d5add46fb42f48060ef971b93d026dcd9c03
Devoitine Celena <celena.devoitine@gmail.com>
2023-11-07 15:12:09 +0100

    création des actions

---
+++ src/classes/actions/AbonnementAction.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace iutnc\touiteur\actions;
+
+class AbonnementAction
+{
+    public function execute(): string
+    {
+        $html = '';
+        if ($this->http_method === 'POST') {
+            echo "<button onclick='action=profil' >Retour</button>";
+            //a faire
+            END;
+        }
+        return $html;
+    }
+}
