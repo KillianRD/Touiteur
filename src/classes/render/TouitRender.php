43f980d4e2700243c3f91e36528c44c5c747c586
Devoitine Celena <celena.devoitine@gmail.com>
2023-11-08 16:52:54 +0100

    correction

--- src/classes/render/TouitRender.php
+++ src/classes/render/TouitRender.php
@@ -5,12 +5,23 @@ use iutnc\touiteur\touit\Touit;
 require_once'vendor/autoload.php';
 
 class TouitRender {
+    /**
+     * @var Touit $touit : Touit à afficher
+     */
     private Touit $touit;
 
+    /**
+     * @param Touit $t : Touit à afficher
+     */
     public function __construct(Touit $t){
         $this->touit = $t;
     }
 
+    /**
+     * Methode pour afficher un touit
+     *
+     * @return string : Renvoie le touit sous forme de HTML
+     */
     public function render(): string {
         $html = "<p>{$this->touit->user->nom}</p>";
         $html .= "<p>@"."{$this->touit->user->pseudo}</p>";
