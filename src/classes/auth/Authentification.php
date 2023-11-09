c71432f304dc08d5eef5e67f092be032cd603c12
Ahmed <ahmedcheklat@outlook.com>
2023-11-09 10:19:19 +0100

    Ajout d'un prenom dans User.php

--- src/classes/auth/Authentification.php
+++ src/classes/auth/Authentification.php
@@ -33,7 +33,7 @@ class Authentification
         $requete->execute();
         $infoUser = $requete->fetch(PDO::FETCH_ASSOC);
 
-        $user = new User($infoUser['id'] ,$infoUser['pseudo'], $infoUser['nom'], $infoUser['email'], $infoUser['role']);
+        $user = new User($infoUser['id'] ,$infoUser['pseudo'], $infoUser['nom'], $infoUser['prenom'], $infoUser['email'], $infoUser['role']);
         $_SESSION['user'] = serialize($user);
     }
 }
