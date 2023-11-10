6df2f60d70ebc694082f5f17c9aca570043b8106
Devoitine Celena <celena.devoitine@gmail.com>
2023-11-10 16:36:05 +0100

    com

--- srcAdmin/classes/dispatch/Dispatcher.php
+++ srcAdmin/classes/dispatch/Dispatcher.php
@@ -64,7 +64,7 @@ class Dispatcher
                     </div>      
                     <nav>
                         <ul>
-                            <a href="?action=profil" class="profil"><img src="./images/profil.png" alt="Personne representant un profil" class="logo_profil">Profil</a>
+                            <a href="?action=profil" class="profil_home"><img src="./images/profil.png" alt="Personne representant un profil" class="logo_profil">Profil</a>
                             <a href="?action=touiter" class="touiter"><img src="./images/touiter.png" alt="message" class="logo_touiter">Touiter</a>              
                         </ul>
                     </nav>
